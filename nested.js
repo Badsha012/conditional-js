8a0f733acf9c5db2f8f6f8b133bf0482915acf32
Badsha012 <badshasheikh361@gmail.com>
2025-07-27 21:28:58 -0700

    seven commit

---
+++ nested.js
@@ -0,0 +1,21 @@
+
+
+const money=200;
+ if(money>300){
+    console.log('bro you ar rich!!');
+ }
+ else{
+    if(money>100){
+        console.log('tui bro lok o na gorib');
+    }
+    else
+    {
+        if(money>0){
+            console,log('dost kpla kha .calcium bara')
+        }
+    
+    else{
+        console.log('tu amer bondu na');
+    }
+}
+ }
