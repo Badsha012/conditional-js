b5c6423c53dab5dc8f70e364daa3d39b7cc9506b
Badsha012 <badshasheikh361@gmail.com>
2025-07-27 19:28:51 -0700

    three commit

---
+++ hotal.js
@@ -0,0 +1,26 @@
+
+
+
+const age=17;
+const price=1000;
+if(age<12){
+    console.log('you  can eat fr free');
+
+}
+else if(age>=60){
+    //50% discount
+    const discount=price*50/100;
+    const payamount=print-discount;
+    console.log(payamount);
+}
+else if(age>=50){
+    //40% discunt
+
+    const  discount=price*25/100;
+    const payamount=price-discount;
+    console.log(package);
+}
+else{
+    console.log('Is full price');
+
+}
