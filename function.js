348df4f6bf8a90c99c2f30b80cec5339abf6e78d
Badsha012 <badshasheikh361@gmail.com>
2025-08-04 07:41:14 -0700

    five commit

--- function.js
+++ function.js
@@ -1,3 +1,12 @@
 function hell(){
-    
+    console.log("I am a student");
+
 }
+hell();
+
+
+function momeybag(){
+    var money=4000;
+    console.log('i hav money' +money+ 'tk');
+}
+momeybag();
