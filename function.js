24c9f6702fef457e66dbc8eccd95e5ee17e43fd8
Badsha012 <badshasheikh361@gmail.com>
2025-08-04 07:49:41 -0700

    seven commit

--- function.js
+++ function.js
@@ -10,3 +10,22 @@ function momeybag(){
     console.log('i hav money' +money+ 'tk');
 }
 momeybag();
+
+function sum(s,r){
+    let sum=s+r;
+    console.log("sum is="+sum);
+}
+sum( 10,40);
+
+
+function add(a,b){
+    let x=a+b;
+    console.log("add="+x);
+    return x;
+}
+   var y=add( 4,7);
+   console.log(y);
+
+
+
+
